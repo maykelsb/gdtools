3cb1300b0a3c2bc7f5b4038c6745b3e943b54120
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-05-25 17:33:43 -0300

    Script de criação de tabelas

---
+++ app/database/creation.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `gdtools`.`project` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL COMMENT 'Project\'s name',
+  `description` TEXT NULL COMMENT 'Project\'s description',
+  `creation` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Project\'s creation date',
+  PRIMARY KEY (`id`))
+COMMENT = 'Store game projects';
+---
+ALTER TABLE `gdtools`.`project`
+RENAME TO  `gdtools`.`projects` ;
+
+
