25e3853616f763d69b659e19b9397a3e3fb3ee46
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-05-27 13:27:24 -0300

    Removendo stub data

--- src/ProjectBundle/Controller/DefaultController.php
+++ src/ProjectBundle/Controller/DefaultController.php
@@ -2,30 +2,28 @@
 
 namespace ProjectBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use FOS\RestBundle\View\View;
 use FOS\RestBundle\Controller\FOSRestController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends FOSRestController
 {
-	protected $data = [
-		1 => ['id' => 1, 'name' => 'Guild Wars', 'description' => 'Um jogo de cartinhas para descobrir quem é o melhor gestor de guildas'],
-		2 => ['id' => 2, 'name' => 'Zankar', 'description' => 'Um tower defense de tabuleiro com uma mecânica bizarra de movimentação'],
-		3 => ['id' => 3, 'name' => 'Dragonaltas', 'description' => 'E então, você é o melhor piloto de dragão?'],
-	];
-
 	public function getProjectsAction()
 	{
+        $projects = $this->getDoctrine()
+            ->getRepository('ProjectBundle:Project')
+            ->findAll();
+
         return $this->handleView(
-            $this->view($this->data, 200)
+            $this->view($projects, 200)
         );
 	}
 
 	public function getProjectAction($id)
 	{
+        $project = $this->getDoctrine()->getRepository('ProjectBundle:Project')
+            ->find($id);
+
 		return $this->handleView(
-            $this->view($this->data[$id], 200)
+            $this->view($project, 200)
         );
 	}
 
