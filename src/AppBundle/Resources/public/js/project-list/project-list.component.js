191710f49e711be90fdeee6768f0f941a3a289d2
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-06-07 13:30:47 -0300

    View using modular angularjs - list of projects

--- src/AppBundle/Resources/public/js/project-list/project-list.component.js
+++ src/AppBundle/Resources/public/js/project-list/project-list.component.js
@@ -1,19 +1,10 @@
 angular.module('projectList').component('projectList', {
     templateUrl: 'js/project-list/project-list.template.html',
-    controller: function ProjectListController(){
-        this.projects = [
-            {
-                name: 'Zankar',
-                description: 'Guardões de Zankar'
-            },
-            {
-                name: 'Guildas',
-                description: 'Fantasy Guilds'
-            },
-            {
-                name: 'Dragonautas',
-                description: 'Pilotos de dragões'
-            }
-        ];
+    controller: function ProjectListController($http){
+        var self = this;
+
+        $http.get('api/projects.json').then(function(response){
+            self.projects = response.data;
+        });
     }
 });
